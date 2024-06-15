c1a1368585b2751d92fc7d2884a35b19a6304e5f
Sherman Devers <67293014+ShermanDevers@users.noreply.github.com>
2024-06-15 13:55:59 -0500

    removed comments

--- default.py
+++ default.py
@@ -1,38 +1,38 @@
 # This goes in ~/.themes/default.py
 
-# from powerline_shell.themes.default import DefaultColor
+from powerline_shell.themes.default import DefaultColor
 
 
-# class Color(DefaultColor):
-#     """Basic theme which only uses colors in 0-15 range"""
-#     USERNAME_FG = 15
-#     USERNAME_BG = 0
-#     USERNAME_ROOT_BG = 1
+class Color(DefaultColor):
+    """Basic theme which only uses colors in 0-15 range"""
+     USERNAME_FG = 15
+     USERNAME_BG = 0
+     USERNAME_ROOT_BG = 1
 
-#     HOSTNAME_FG = 15
-#     HOSTNAME_BG = 0
+     HOSTNAME_FG = 15
+     HOSTNAME_BG = 0
 
-#     HOME_SPECIAL_DISPLAY = False
-#     PATH_BG = 0  # dark grey
-#     PATH_FG = 15  # light grey
-#     CWD_FG = 15  # white
-#     SEPARATOR_FG = 15
+     HOME_SPECIAL_DISPLAY = False
+     PATH_BG = 0  # dark grey
+     PATH_FG = 15  # light grey
+     CWD_FG = 15  # white
+     SEPARATOR_FG = 15
 
-#     READONLY_BG = 0
-#     READONLY_FG = 15
+     READONLY_BG = 0
+     READONLY_FG = 15
 
-#     REPO_CLEAN_BG = 2   # green
-#     REPO_CLEAN_FG = 0   # black
-#     REPO_DIRTY_BG = 1   # red
-#     REPO_DIRTY_FG = 15  # white
+     REPO_CLEAN_BG = 2   # green
+     REPO_CLEAN_FG = 0   # black
+     REPO_DIRTY_BG = 1   # red
+     REPO_DIRTY_FG = 15  # white
 
-#     JOBS_FG = 1
-#     JOBS_BG = 0
+     JOBS_FG = 1
+     JOBS_BG = 0
 
-#     CMD_PASSED_BG = 0
-#     CMD_PASSED_FG = 15
-#     CMD_FAILED_BG = 0
-#     CMD_FAILED_FG = 15
+     CMD_PASSED_BG = 0
+     CMD_PASSED_FG = 15
+     CMD_FAILED_BG = 0
+     CMD_FAILED_FG = 15
 
-#     SVN_CHANGES_BG = REPO_DIRTY_BG
-#     SVN_CHANGES_FG = REPO_DIRTY_FG
+     SVN_CHANGES_BG = REPO_DIRTY_BG
+     SVN_CHANGES_FG = REPO_DIRTY_FG
